8e5da2da4bbc4f5fdc98f6ddbfc58ce171d92305
ngocduc202 <ngocduc1542002@gmail.com>
2024-11-13 23:44:08 +0700

    create Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
+import { Outlet } from 'react-router-dom'
+import "./App.css"
+import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello
+      <Navbar />
+      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+        <Outlet />
+      </main>
+      <footer>foot</footer>
     </>
   )
 }
