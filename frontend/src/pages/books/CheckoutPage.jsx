637d6a7ce496d362d422f9c93587e3f4804030e1
ngocduc202 <ngocduc1542002@gmail.com>
2024-11-17 17:13:29 +0700

    create Protected Route

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
 
 const CheckoutPage = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm()
   const [isChecked, setIsChecked] = useState(false)
   const { cartItems } = useSelector(state => state.cart)
   const totalPrice = cartItems?.reduce((acc, item) => acc + item.newPrice, 0).toFixed(2)
-  const currentUser = true
+  const { currentUser } = useAuth()
   const onSubmit = (data) => {
     const newOrder = {
       name: data.name,
